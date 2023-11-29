da140b0b4337dd4f24df1eb6bea2d04df8c2a7f4
manana <jredondop@outlook.es>
2023-11-29 14:42:15 +0100

    app terminada

--- app/src/main/java/com/example/app_tema1_fredpor/chistes.java
+++ app/src/main/java/com/example/app_tema1_fredpor/chistes.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,7 @@ public class chistes extends AppCompatActivity implements TextToSpeech.OnInitLis
         Button volverMenu = findViewById(R.id.volverMenu);
         Button generarChiste = findViewById(R.id.generarChiste);
         TextView texto_chiste = findViewById(R.id.text_chistes);
+        Spinner spinner = findViewById(R.id.elegirTema);
 
         volverMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,27 +38,59 @@ public class chistes extends AppCompatActivity implements TextToSpeech.OnInitLis
         generarChiste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] chistes = {
-                        "¿Cuál es el animal más antiguo?\nLa cebra, porque está en blanco y negro.",
-                        "PACO, dime algo que me haga sentir muy perra\nSIT MARIA, SIT",
-                        "¿Por qué los programadores prefieren el verano?\nPorque les gusta tener ventanas abiertas.",
-                        "¿Cuántos programadores se necesitan para cambiar una bombilla?\nNinguno, es un problema de hardware.",
-                        "Papá, papá, porque me echas güacamole por encima?\nque te calles nacho",
-                        "¿Cuál es el lenguaje favorito de los programadores?\nEl lenguaje de señas.",
-                        "POR EL CULO NO PACO, POR EL CULO NO\nPues ya me diras tu manolo...",
-                        "Mire doctor, esque solo oigo por un oido, por el otro no\nNo oye por un oido? a ver diga 100\n50",
-                        "Manuel, tu coche es automatico?\nEs manual\nManual, tu coche es automatico?.",
-                        "Papa hace 4 meses te robaron la tarjeta y no lo has denunciao\nYa hijo, esque el ladron gasta menos que tu madre.",
-                        "¿Cómo se dice 'hola' en lenguaje binario?\n01001000 01101111 01101100 01100001.",
-                        "Hola guapa, si adivinas un numero del 1 al 5 nos tomamos algo juntos\nel 3\nPOR UNO TIO, JODER, POR UNO",
-                        "¿Cómo arreglan los programadores la lámpara del techo?\nLa apagan y la vuelven a encender.",
-                        "Van dos ciegos y uno le dice a otro\nOjala lloviera\nOjala yo tambien...",
-                        "En que se parecen una bala y una cueva?\nEn que en la cueva hay humedad\ny la bala humedad u no meda",
-                        "Mira una serpiente reptando por el pasillo!!!\n5-2=3, 9-5=4...",
-                        "Entra un huerfano a una pizzeria\nBuenas, quisiera dos familiares...",
+                String temaSeleccionado = spinner.getSelectedItem().toString();
+                String[] chistes = {};
+                switch (temaSeleccionado){
+                    case "JAIMITO":
+                        chistes = new String[]{
+                                "Jaimito, te has copiado del examen de Pedro?\nNo maestra\nY entonces porque en la pregunta 4, donde Pedro contesta 'No lo se' tu pones 'Yo tampoco'?",
+                                "Jaimito, cual es la definicio de telepatía?\nUna televisión pa la hermana de mi madre",
+                                "Jaimito, la M con la A?\nMA\nAhora repetido\nMAMA\nahora con tilde\nMAtilde",
+                                "Jaimito, dime 5 animales africanos\n 5 elefantes profe",
+                                "Profe, como se dice nariz en inglés?\nnose jaimito\njoe pues vaya profesora...",
+                                "Papá, donde esta Rusia?\nNo se preguntale a tu madre",
+                                "Jaimito, cuantas anclas tiene un barco?\n once profe\n porque once?\n porque siempre dicen eleven anclas...",
+                                "Jaimito, que es una orilla?\n Sesenta minutillos"
+                        };
 
-                };
+                        break;
+                    case "INFORMATICOS":
+                        chistes = new String[] {
+                                "¿Me puede decir la contraseña de wifi de su bar, por favor?\nTómate un agua por lo menos, ¿no?\n¿Todo junto?",
+                                "Mi mujer me dijo que necesitaba más espacio.\n¿Y qué hiciste?\nLe regalé un disco duro de 2Tb.",
+                                "¿Por qué los programadores prefieren el verano?\nPorque les gusta tener ventanas abiertas.",
+                                "¿Cuántos programadores se necesitan para cambiar una bombilla?\nNinguno, es un problema de hardware.",
+                                "Hola, llamo porque no me funciona el módem de internet.\n¿Qué luces tiene encendidas?\nLa del pasillo y la del salón\nVale, ahora mismo le envío a un informático.",
+                                "¿Cuál es el lenguaje favorito de los programadores?\nEl lenguaje de señas.",
+                                "¿Cómo se dice 'hola' en lenguaje binario?\n01001000 01101111 01101100 01100001.",
+                                "¿Cómo arreglan los programadores la lámpara del techo?\nLa apagan y la vuelven a encender.",
+                                "Google es como un cuñado: no te deja acabar la frase y ya te está dando sugerencias",
+                                "Me pone que mi contraseña es incorrecta, pero escribo 'incorrecta' y no me deja entrar"
+                        };
+                        break;
+                    case "DOCTOR":
+                        chistes = new String[]{
+                                "Mire doctor, esque solo oigo por un oido, por el otro no\nNo oye por un oido? a ver diga 100\n50",
+                                "Doctor, doctor, tengo paperas\n Pues toma 2euros y ya tienes paplatanos",
 
+
+
+
+                        };
+                        break;
+                    case "RANDOM":
+                        chistes = new String[]{
+                                "Papá, papá, porque me echas güacamole por encima?\nque te calles nacho",
+                                "Manuel, tu coche es automatico?\nEs manual\nManual, tu coche es automatico?.",
+                                "Papa hace 4 meses te robaron la tarjeta y no lo has denunciao\nYa hijo, esque el ladron gasta menos que tu madre.",
+                                "Hola guapa, si adivinas un numero del 1 al 5 nos tomamos algo juntos\nel 3\nPOR UNO TIO, JODER, POR UNO",
+                                "Van dos ciegos y uno le dice a otro\nOjala lloviera\nOjala yo tambien...",
+                                "En que se parecen una bala y una cueva?\nEn que en la cueva hay humedad\ny la bala humedad u no meda",
+                                "Mira una serpiente reptando por el pasillo!!!\n5-2=3, 9-5=4...",
+                                "Entra un huerfano a una pizzeria\nBuenas, quisiera dos familiares...",
+                        };
+                        break;
+                }
                 String chisteElegido = elegirChiste(chistes);
 
                 texto_chiste.setText(chisteElegido);
