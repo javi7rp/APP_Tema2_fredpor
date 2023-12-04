27c154a987afb1fc948b2c5b743971623391bf69
manana <jredondop@outlook.es>
2023-12-04 09:35:18 +0100

    app terminada

--- app/src/main/java/com/example/app_tema1_fredpor/LLamadaActivity.kt
+++ app/src/main/java/com/example/app_tema1_fredpor/LLamadaActivity.kt
@@ -11,6 +11,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -30,11 +31,10 @@ class LLamadaActivity : AppCompatActivity() {
         val volverMenu = findViewById<Button>(R.id.volverMenu)
 
         botonRealizarLLamada.setOnClickListener {
-            val telefono = "tel: 112"
-            val intent = Intent(Intent.ACTION_CALL, Uri.parse(telefono))
+            val intentLargo = Intent(Intent.ACTION_CALL, Uri.parse("tel:androidx.appcompat.widget.AppCompatTextView{112 app:id/textoLLamada}"))
 
             if (ContextCompat.checkSelfPermission(this, CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                startActivity(intent)
+               startActivity(intentLargo)
             } else {
                 ActivityCompat.requestPermissions(this, arrayOf(CALL_PHONE), REQUEST_CODE_LLAMADA)
             }
@@ -54,15 +54,11 @@ class LLamadaActivity : AppCompatActivity() {
 
         if (requestCode == REQUEST_CODE_LLAMADA) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                val telefono = "tel:112"
-                val intent = Intent(Intent.ACTION_CALL, Uri.parse(telefono))
-                startActivity(intent)
+                val intentLargo = Intent(Intent.ACTION_CALL, Uri.parse("tel:androidx.appcompat.widget.AppCompatTextView{112 app:id/textoLLamada}"))
+                startActivity(intentLargo)
             } else {
                 Toast.makeText(this, "Los permisos de llamada son necesarios para realizar una llamada.", Toast.LENGTH_SHORT).show()
             }
         }
     }
-
-
-
 }
